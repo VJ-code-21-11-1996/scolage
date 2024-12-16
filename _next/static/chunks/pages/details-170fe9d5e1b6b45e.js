5d97d8cba7cd7e631cd01bb704e968d4212732b2
Vishal Jalondhara <savanitaliya356@gmail.com>
2024-12-16 16:36:17 +0530

    first commit

---
+++ _next/static/chunks/pages/details-170fe9d5e1b6b45e.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[354],{8020:function(n,u,i){(window.__NEXT_P=window.__NEXT_P||[]).push(["/details",function(){return i(7723)}])},7723:function(n,u,i){"use strict";i.r(u);var t=i(5893);i(7294),u.default=()=>(0,t.jsx)("div",{children:"details"})}},function(n){n.O(0,[774,888,179],function(){return n(n.s=8020)}),_N_E=n.O()}]);
